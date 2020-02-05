3ed3793ee5a1b05d75a34f70cb7f546e8457405d
Muchammad Arsyad <arsyad1822@gmail.com>
2020-02-05 09:04:41 +0700

    pertemuan1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Table} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+
+    //   </header>
+    // </div>
+  
+  <div className="App">
+  <header>
+    <Navbar expand="lg" variant="dark" bg="dark">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Feature</Nav.Link>
+              <Nav.Link href="#link">Pricing</Nav.Link>
+              <Nav.Link href="#link">Disable</Nav.Link>
+          </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+      <img src={logo} className="App-logo" alt="logo" />
+  </header>
+  
+    <div className="row mb-4">
+      <div className="col-sm-12 grid-margin">
+        <div className="card h-100">
+          <div className="card-body">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Larry</td>
+                  <td>The Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+
   );
 }
 
